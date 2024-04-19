8e3a1b27b44131028ca26999eb2c0bd75465f680
hacker777-cris <crispusgikonyo@gmail.com>
2024-04-19 12:02:54 +0300

    add static url path

--- samantha_campaigns/urls.py
+++ samantha_campaigns/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
 
 from . import views
 
 
 urlpatterns = [
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
     path('samanta-campaigns/', views.health_check_api_view, name="health-check"),
     path('samanta-campaigns/admin/', admin.site.urls),
     path("samanta-campaigns/api/v1/", include("api.urls"), name="api"),
